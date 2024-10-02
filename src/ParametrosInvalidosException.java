9f94db289c84fef33d92f84ee6c45263834b8f7c
edulourenzo <edulourenzo@hotmail.com>
2024-10-02 18:36:50 -0300

    Commit inicial

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException() {
+        System.err.println("O segundo parâmetro deve ser maior que o primeiro");
+    }
+
+}
